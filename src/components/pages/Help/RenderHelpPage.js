f267e583cd2eb06b5ee75b44a46591aa272b8218
unknown <abdinajibmohamud@gmail.com>
2020-08-27 11:26:15 -0700

    added steps to help page with folder with gifs and step 1-2

--- src/components/pages/Help/RenderHelpPage.js
+++ src/components/pages/Help/RenderHelpPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// import stepone from '../../../assets/steps/stepone.gif'
 
 function RenderHelpPage(props) {
   return (
@@ -17,7 +18,31 @@ function RenderHelpPage(props) {
         </nav>
       </header>
       <div className="content-container">
-        <h1>Help</h1>
+        <div className="steps">
+          <div className="stepone">
+            <h1>STEP 1</h1>
+            <h2>Register for an account.</h2>
+            <img
+              src={require('../../../assets/steps/stepone.gif')}
+              alt="stepone"
+            />
+          </div>
+          <div className="steptwoandthree">
+            <h1>STEP 2</h1>
+            <h2>Log in.</h2>
+            <img
+              src={require('../../../assets/steps/steptwo.gif')}
+              alt="steptwo"
+            />
+            <br></br>
+            <h1>STEP 3</h1>
+            <h2>Create a New Event.</h2>
+            <img
+              src={require('../../../assets/steps/stepthree.gif')}
+              alt="stepthree"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
